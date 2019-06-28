5c85c5781977d5dfed12dd7606b320e15d4c0162
de-atmosfeer <44024695+de-atmosfeer@users.noreply.github.com>
2019-06-28 15:10:29 +0200

    Klaas vs claus Atmosfeest 5/12/18

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ from flask import Flask
 import numpy as np
 from datapakker import *
 import time
-from commissielezer import *
 import sys
-from spotifyunit import *
+import math
 
 app = Flask(__name__)
 
@@ -20,25 +19,19 @@ sys.maxsize = 2018302999999
 
 #Current year, month, day, hour, minute (needed to request data from Twelve)
 dy, dm, dd, dh, dn = time.localtime(time.time())[0:5]
-dh = dh-6
+dh = dh-2
 statistiekenaan = 1
-turfdictalcohol = turflijst()
-turfdictfris = turflijst()
+sintzuipt = 1
+kerstmanzuipt = 1
+productsint = 788436
+productkerstman = 788437	
 testing = 0
-
-@app.route('/index/spotify/')
-def spotifyding():
-    informatie = info()
-    sequence = "<table><tr><th width=30% style='padding: 0px;' rowspan = 2><img src='" + informatie['plaatje'] + "' width=100px></th><th id='CurrPlay'>Currently Playing:</th></tr>"
-    sequence += "<tr>"
-    sequence += "<th id='MusicInfo'>" + informatie['artiest'] + "<br>" + informatie['nummer'] + "</th>"
-    return sequence#"Artiest: " + informatie['artiest'] + "<br>Nummer: " + informatie['nummer'] + "<br><img src='" + informatie['plaatje'] + "'>"
+hoevaak = 50
 
 @app.route('/index/daemon/')
 def statistiekendeamon():
-    global dy, dm, dd, dh, dn, turfdictalcohol, turfdictfris, testing
-    commissiedict = commissielijst()
-    commissiedict[''] = ''
+    global dy, dm, dd, dh, dn, productsint, productkerstman, testing, sintzuipt, kerstmanzuipt, hoevaak
+    hoevaak += 1
     if testing == 0:
         py, pm, pd, ph, pn = dy, dm, dd, dh, dn
         dy, dm, dd, dh, dn = time.localtime(time.time())[0:5]
@@ -60,6 +53,7 @@ def statistiekendeamon():
             teller += 1
     
     startpos = teller
+    modifier = hoevaak/50
     
     running = 1
     if foundstart == 0:
@@ -72,47 +66,25 @@ def statistiekendeamon():
         else:
             naam = rawdata[teller+6 + 26*i].strip("\t<td>").strip("</td>\n")
             hoeveel = rawdata[teller+16 + 26*i].strip('\t<td align="right">').strip("</td>\n")
-            product = rawdata[teller+23 + 26*i].strip("\t<td>").strip("</td>\n") #print naam + " en die kocht " + hoeveel + " " + product
-            if '2018302' in naam:
-                naam = long(naam) - 2018302000000 + 4302000
-            if naam == '': #Make sure pin or cash doesnt crash the system
-                pass
-            elif int(naam) in commissiedict and commissiedict[int(naam)] != '':
-		if commissiedict[int(naam)] not in turfdictalcohol:
-                    turfdictalcohol[commissiedict[int(naam)]] = 0
-                    turfdictfris[commissiedict[int(naam)]] = 0
-                if int(product) == 8010:
-                    turfdictalcohol[commissiedict[int(naam)]] += int(hoeveel)
-                if int(product) == 8020:
-                    turfdictfris[commissiedict[int(naam)]] += int(hoeveel)
-            i += 1 
+            product = rawdata[teller+14 + 26*i].strip("\t<td>").strip("</td>\n") #print naam + " en die kocht " + hoeveel + " " + product
+            if int(product) == productsint:
+                sintzuipt += int(hoeveel)*modifier
+            if int(product) == productkerstman:
+                kerstmanzuipt += int(hoeveel)*modifier
+        i += 1 
+    omrekenfactor = 1200./(sintzuipt+kerstmanzuipt)
+    kerstmanbalkje = math.floor(kerstmanzuipt*omrekenfactor)
+    sintbalkje = math.ceil(sintzuipt*omrekenfactor)
+	
+    print "Sint: " + str(sintzuipt) +", Kerstman: " + str(kerstmanzuipt) + " Debug infomartie: " + str(hoevaak) + " " + str(modifier)
     
-    lijstalcohol = sorted(turfdictalcohol.iteritems(), key=lambda (k,v): (v,k), reverse = True)
-    lijstfris = sorted(turfdictfris.iteritems(), key=lambda (k,v): (v,k), reverse = True)
+    sequence = '<table><tr width="1200px" height="120px"><td width='+ str(sintbalkje) +'px" style="background-color:rgba(193,13,0,0.85);"> </td><td width=' + str(kerstmanbalkje) + 'px" style="background-color:rgba(0,193,13,0.85);"> </td></tr></table>' 
 
-    sequence = "<table><tr><th width=40%>Alcoholic:</th><th width=10%></th><th width=40%>Non-alcoholic:</th><th width=10%></th></tr>\n"
-    for i in range(len(lijstalcohol)):
-        sequence += "<tr>\n"
-        if lijstalcohol[i][1] != 0:
-            sequence += "<td>" + str(lijstalcohol[i][0]) + "</td><td width=10%>" + str(lijstalcohol[i][1]) + "</td>\n"
-        else:
-            sequence += "<td></td><td></td>\n"
-        if lijstfris[i][1] != 0:
-            sequence += "<td>" + str(lijstfris[i][0]) + "</td><td width=10%>" + str(lijstfris[i][1]) + "</td>\n"
-        else:
-            sequence += "<td></td><td></td>\n"
-        sequence += "</tr>\n"
-    sequence += "</table>"
     return sequence        
     
 @app.route('/index/')
 def indexpage():
     page = open('www/page.html')
-    return page.read()
-
-@app.route('/prijslijst/')
-def prijslijstpage():
-    page = open('www/geenstatistiek.html')
     return page.read()
         
 @app.route('/index/refresher.js')
