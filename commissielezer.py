a44389f0d1350e35753402583b5506b5c7c8534b
Niels Mateijsen <n.mateijsen@student.tudelft.nl>
2021-01-11 14:39:01 +0100

    Encoding toegevoegd

--- commissielezer.py
+++ commissielezer.py
@@ -6,7 +6,7 @@ import numpy as np
 def commissielijst():
     try:
         c_lijst = np.genfromtxt(os.path.dirname(os.path.realpath(__file__)) + "/rekeningen.csv", dtype=None,
-                                skip_header=1, delimiter=";")
+                                skip_header=1, delimiter=";", encoding='windows-1252')
     except ConnectionError:
         print("ERROR: GEEN INTERNET")
         return {}
